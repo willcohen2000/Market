ce8af588629d3a6c11d4348a30031ce1534d6075
willcohen2000 <willcohen2000@gmail.com>
2017-10-08 10:38:13 -0400

    Initialization of files

---
+++ MarketAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarketAPP
+//
+//  Created by Will Cohen on 10/8/17.
+//  Copyright © 2017 Will Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
