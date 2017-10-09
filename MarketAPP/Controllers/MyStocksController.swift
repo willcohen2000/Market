a16ddd53e9284905e5daaa7c260cf296f589d62a
willcohen2000 <willcohen2000@gmail.com>
2017-10-09 19:30:08 -0400

    Began creating 'my stocks' page

---
+++ MarketAPP/Controllers/MyStocksController.swift
@@ -0,0 +1,46 @@
+//
+//  MyStocksController.swift
+//  MarketAPP
+//
+//  Created by Will Cohen on 10/9/17.
+//  Copyright © 2017 Will Cohen. All rights reserved.
+//
+
+import UIKit
+
+class MyStocksController: UIViewController {
+
+    @IBOutlet weak var allStocksSelected: UIImageView!
+    @IBOutlet weak var shortStocksSelected: UIImageView!
+    @IBOutlet weak var longStocksSelected: UIImageView!
+    @IBOutlet weak var myStocksTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func allStocksButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortStocksButtonPressed(_ sender: Any) {
+        
+    }
+
+    @IBAction func longStocksButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addNewStockButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signOutButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
+
+
+
+
