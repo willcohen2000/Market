175a4d77728ff2cc4c06278d914c2f6b878b23d8
willcohen2000 <willcohen2000@gmail.com>
2017-10-08 20:15:26 -0400

    Completed first feature w/ graph

--- MarketAPP/Views/GraphFeatureDisplayView.swift
+++ MarketAPP/Views/GraphFeatureDisplayView.swift
@@ -14,8 +14,8 @@ import UIKit
     
     private struct Constants {
         static let margin: CGFloat = 0.0
-        static let topBorder: CGFloat = 60
-        static let bottomBorder: CGFloat = 50
+        static let topBorder: CGFloat = 10
+        static let bottomBorder: CGFloat = 20
         static let colorAlpha: CGFloat = 0.3
         static let circleDiameter: CGFloat = 5.0
     }
@@ -23,6 +23,14 @@ import UIKit
     override func draw(_ rect: CGRect) {
         let width = rect.width
         let height = rect.height
+        
+        //CREATE GRAPH POINTS
+        let context = UIGraphicsGetCurrentContext()!
+        let colors = [UIColor(red:0.28, green:0.15, blue:0.42, alpha:1.0).cgColor, UIColor(red: 0, green: 0, blue: 0, alpha: 0).cgColor]
+        let colorLocations: [CGFloat] = [0.0, 1.0]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let gradient = CGGradient(colorsSpace: colorSpace, colors: colors as CFArray, locations: colorLocations)!
+        
         
         let graphWidth = width
         let columnXPoint = { (column: Int) -> CGFloat in
@@ -49,6 +57,20 @@ import UIKit
             let nextPoint = CGPoint(x: columnXPoint(point), y: columnYPoint(graphPoints[point]))
             graphPath.addLine(to: nextPoint)
         }
+        
+        // CREATE GRADIENT
+        let clippingPath = graphPath.copy() as! UIBezierPath
+        clippingPath.addLine(to: CGPoint(x: columnXPoint(graphPoints.count - 1), y: height))
+        clippingPath.addLine(to: CGPoint(x: columnXPoint(0), y: height))
+        clippingPath.close()
+        clippingPath.addClip()
+        
+        let highestYPoint = columnYPoint(maxValue)
+        let graphStartPoint = CGPoint(x: 0, y: highestYPoint)
+        let graphEndPoint = CGPoint(x: 0, y: bounds.height)
+        context.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options: [])
+        
+        graphPath.lineWidth = 0.0
         graphPath.stroke()
         
     }
