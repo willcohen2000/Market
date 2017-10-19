4d45a5a1fd2030322218fc5b99dac4f4fd8b9dc0
willcohen2000 <willcohen2000@gmail.com>
2017-10-18 22:02:18 -0400

    Added new stock page and created outlets/actions

---
+++ MarketAPP/Controllers/AddNewStockController.swift
@@ -0,0 +1,37 @@
+//
+//  AddNewStockController.swift
+//  MarketAPP
+//
+//  Created by Will Cohen on 10/18/17.
+//  Copyright © 2017 Will Cohen. All rights reserved.
+//
+
+import UIKit
+
+class AddNewStockController: UIViewController {
+
+    @IBOutlet weak var addStockButton: UIButton!
+    @IBOutlet weak var numberOfSharesLabel: UILabel!
+    @IBOutlet weak var pricePerShareLabel: UILabel!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var tickerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addStuckButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func longButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func shortButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
--- MarketAPP/Controllers/StockAnalyticsController.swift
+++ MarketAPP/Controllers/StockAnalyticsController.swift
@@ -24,28 +24,66 @@ class StockAnalyticsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeView()
-        getStockAnalytics(ticker: "MSFT")
+        getStockAnalytics(ticker: "SNAP")
     }
     
     private func getStockAnalytics(ticker: String) {
-        let stockAnalyticsJSONUrl: String = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=\(ticker)&interval=15min&outputsize=full&apikey=demo"
+        let stockAnalyticsJSONUrl: String = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=\(ticker)&interval=15min&outputsize=compact&apikey=80URCYB65S5YLJKI"
         Alamofire.request(URL(string: stockAnalyticsJSONUrl)!, method: .get).validate().responseJSON { (response) in
             switch response.result {
             case .success(let value):
                 let stockAnalyticJSON = JSON(value)["Time Series (15min)"]
+                var lastTradingDay: String = ""
                 for (stockInfoAtTime):(String, JSON) in stockAnalyticJSON {
-                    if let stockPrice = Float(stockInfoAtTime.1["4. close"].stringValue) {
-                        self.stockPriceGraphPoints.insert(stockPrice, at: 0)
-                    } else {
-                        print("ERROR!: \(stockInfoAtTime.1["4. close"])")
+                    if (lastTradingDay.isEmpty) {
+                        lastTradingDay = self.getDayOfTrade(date: stockInfoAtTime.0)
                     }
+                    if (lastTradingDay == self.getDayOfTrade(date: stockInfoAtTime.0)) {
+                        if let stockPrice = Float(stockInfoAtTime.1["4. close"].stringValue) {
+                            self.stockPriceGraphPoints.insert((stockPrice), at: 0)
+                        } else {
+                            print("ERROR!: \(stockInfoAtTime.1["4. close"])")
+                        }
+                    } 
                 }
-                let stockAnalayticsGraph = StockAnalyticsGraphView(graphPointsInit: self.stockPriceGraphPoints, frame: self.stockAnalayticsGraphHolderView.frame)
+                let stockAnalayticsGraph = StockAnalyticsGraphView(graphPointsInit: self.graphPointsFit(graphPoints: self.stockPriceGraphPoints), frame: self.stockAnalayticsGraphHolderView.frame)
                 self.view.addSubview(stockAnalayticsGraph)
             case .failure(let error):
                 print(error)
             }
         }
+    }
+    
+    private func graphPointsFit(graphPoints: [Float]) -> [Float] {
+        var lowestPoint: Float = 0.0;
+        var fittedGraphPoints: [Float] = []
+        for (point) in graphPoints {
+            if (lowestPoint == 0) {
+                lowestPoint = point
+            } else {
+                if (point < lowestPoint) {
+                    lowestPoint = point
+                }
+            }
+        }
+        for (point) in graphPoints {
+            fittedGraphPoints.append(point - (lowestPoint - 1))
+        }
+        return fittedGraphPoints
+    }
+    
+    private func getDayOfTrade(date: String) -> String {
+        var i = 0
+        var abridgedDay: String = ""
+        for (character) in date {
+            if (i == 10) {
+                break;
+            } else {
+                abridgedDay = "\(abridgedDay)\(character)"
+            }
+            i+=1
+        }
+        return abridgedDay
     }
     
     private func initializeView() {
